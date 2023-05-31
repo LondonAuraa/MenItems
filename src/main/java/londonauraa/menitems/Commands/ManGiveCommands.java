ae7e40042d9007276124d03f127c54b4bd8436e3
LondonAuraa <91578335+LondonAuraa@users.noreply.github.com>
2023-05-30 20:05:28 -0400

    1.2

--- src/main/java/londonauraa/menitems/Commands/ManGiveCommands.java
+++ src/main/java/londonauraa/menitems/Commands/ManGiveCommands.java
@@ -1,13 +1,19 @@
 package londonauraa.menitems.Commands;
 
+import londonauraa.menitems.Items.Items;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import static londonauraa.menitems.Items.Items.*;
 
 public class ManGiveCommands implements CommandExecutor {
+    Items itemlists = new Items();
+    ItemStack manRod = itemlists.returnItem(1);
+    ItemStack manLauncher = itemlists.returnItem(2);
+    ItemStack manSwarmEgg = itemlists.returnItem(0);
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
@@ -15,13 +21,13 @@ public class ManGiveCommands implements CommandExecutor {
             Player player = (Player) sender;
             if (player.hasPermission("manitems.mangive")){
                 if(args[0].equalsIgnoreCase("MermanRod")){
-                    player.getInventory().addItem(createManRod());
+                    player.getInventory().addItem(manRod);
 
                 }else if (args[0].equalsIgnoreCase("manswarmegg")){
-                    player.getInventory().addItem(createManSwarmEgg());
+                    player.getInventory().addItem(manSwarmEgg);
 
                 }else if(args[0].equalsIgnoreCase("manlauncher")){
-                    player.getInventory().addItem(createManLauncher());
+                    player.getInventory().addItem(manLauncher);
                 }else{
                     return false;
                 }
