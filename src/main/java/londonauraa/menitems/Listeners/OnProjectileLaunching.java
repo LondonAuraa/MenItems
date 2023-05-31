ace067ee2c16ac7d0b6ae50a6b985c8fa64384f6
LondonAuraa <91578335+LondonAuraa@users.noreply.github.com>
2023-05-30 20:07:51 -0400

    Update OnProjectileLaunching.java

--- src/main/java/londonauraa/menitems/Listeners/OnProjectileLaunching.java
+++ src/main/java/londonauraa/menitems/Listeners/OnProjectileLaunching.java
@@ -5,20 +5,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.ItemStack;
 
-import static londonauraa.menitems.Items.Items.manSwarmEgg;
 import static londonauraa.menitems.Listeners.ManSwarmEggSwarm.eggs;
 
 public class OnProjectileLaunching implements Listener {
-
+    Items itemlists = new Items();
+    ItemStack manSwarmEgg = itemlists.returnItem(0);
     @EventHandler
     public void onProjectileLaunch(ProjectileLaunchEvent event){
         if(event.getEntity().getShooter() instanceof Player){
             Player player = (Player) event.getEntity().getShooter();
-            if(player.getInventory().getItemInMainHand().isSimilar(Items.createManSwarmEgg())){
+            if(player.getInventory().getItemInMainHand().isSimilar(manSwarmEgg)){
                 eggs.add(event.getEntity());
             }
         }
 
     }
 }
+
